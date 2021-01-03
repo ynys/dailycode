6734b99d6bce10906adeb0353db3c0ade5980739
Sen <imyangsen@gmail.com>
2021-01-03 14:21:45 -0500

    1/3/2021

---
+++ src/com/leetcode/tree/Leet653.java
@@ -0,0 +1,61 @@
+package com.leetcode.tree;
+
+import java.util.*;
+
+public class Leet653 {
+    //BST
+    public boolean findTarget1(TreeNode root, int k) {
+        List< Integer > list = new ArrayList<>();
+        inorder(root, list);
+        int l = 0, r = list.size() - 1;
+        while (l < r) {
+            int sum = list.get(l) + list.get(r);
+            if (sum == k)
+                return true;
+            if (sum < k)
+                l++;
+            else
+                r--;
+        }
+        return false;
+    }
+    public void inorder(TreeNode root, List < Integer > list) {
+        if (root == null)
+            return;
+        inorder(root.left, list);
+        list.add(root.val);
+        inorder(root.right, list);
+    }
+
+    //HashSet + DFS
+    public boolean findTarget2(TreeNode root, int k) {
+        HashSet<Integer> set = new HashSet<>();
+        return dfs(root, set, k);
+    }
+
+    public boolean dfs(TreeNode root, HashSet<Integer> set, int k){
+        if(root == null)return false;
+        if(set.contains(k - root.val))return true;
+        set.add(root.val);
+        return dfs(root.left, set, k) || dfs(root.right, set, k);
+    }
+
+    //HashSet + BFS
+    public boolean findTarget3(TreeNode root, int k) {
+        Set< Integer > set = new HashSet<>();
+        Queue< TreeNode > queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            if (queue.peek() != null) {
+                TreeNode node = queue.remove();
+                if (set.contains(k - node.val))
+                    return true;
+                set.add(node.val);
+                queue.add(node.right);
+                queue.add(node.left);
+            } else
+                queue.remove();
+        }
+        return false;
+    }
+}
