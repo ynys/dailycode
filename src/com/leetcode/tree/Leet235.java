ad5a2ffaa2118ba741bf353dca62c236c2e81981
Sen <imyangsen@gmail.com>
2021-01-19 00:48:31 -0500

    1/19/2021

---
+++ src/com/leetcode/tree/Leet235.java
@@ -0,0 +1,9 @@
+package com.leetcode.tree;
+
+public class Leet235 {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        while ((root.val - p.val) * (root.val - q.val) > 0)
+            root = p.val < root.val ? root.left : root.right;
+        return root;
+    }
+}
